d0e6dbf8a306ea5e0a558e7267774bcbfb35b260
Adrian Braemer <11058200+abraemer@users.noreply.github.com>
2024-11-12 09:54:47 +0100

    cTWA paper in bw

--- papers-bw/merge.sh
+++ papers-bw/merge.sh
@@ -1,2 +1,3 @@
 #!/usr/bin/bash
 pdfunite pub-Braemer2022-Pairlocalization-bw{1,2,3,4,5,6,7,8,9,10}.pdf ../pub-Braemer2022-Pairlocalization.pdf
+pdfunite pub-Braemer2024-cTWA-bw{1,2,3,4,5,6,7,8,9,10,11,12,13,14}.pdf ../pub-Braemer2024-cTWA.pdf
