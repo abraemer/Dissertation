90d05a5ce07af88ab8be0ae20273bef71c001f56
Adrian Braemer <11058200+abraemer@users.noreply.github.com>
2024-11-12 10:14:42 +0100

    universal-dynamics paper in bw

--- papers-bw/merge.sh
+++ papers-bw/merge.sh
@@ -1,3 +1,4 @@
 #!/usr/bin/bash
 pdfunite pub-Braemer2022-Pairlocalization-bw{1,2,3,4,5,6,7,8,9,10}.pdf ../pub-Braemer2022-Pairlocalization.pdf
 pdfunite pub-Braemer2024-cTWA-bw{1,2,3,4,5,6,7,8,9,10,11,12,13,14}.pdf ../pub-Braemer2024-cTWA.pdf
+pdfunite pub-Franz2023-UniversalDynamics-bw{1,2,3,4,5,6,7,8,9,10,11,12}.pdf ../pub-Franz2023-UniversalDynamics.pdf
