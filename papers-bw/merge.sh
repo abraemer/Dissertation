097f70817fd9bdb5b53726cc5d130eebe2c9c8b8
Adrian Braemer <11058200+abraemer@users.noreply.github.com>
2024-11-18 09:35:13 +0100

    metronome spin turned bw

--- papers-bw/merge.sh
+++ papers-bw/merge.sh
@@ -2,3 +2,6 @@
 pdfunite pub-Braemer2022-Pairlocalization-bw{1,2,3,4,5,6,7,8,9,10}.pdf ../pub-Braemer2022-Pairlocalization.pdf
 pdfunite pub-Braemer2024-cTWA-bw{1,2,3,4,5,6,7,8,9,10,11,12,13,14}.pdf ../pub-Braemer2024-cTWA.pdf
 pdfunite pub-Franz2023-UniversalDynamics-bw{1,2,3,4,5,6,7,8,9,10,11,12}.pdf ../pub-Franz2023-UniversalDynamics.pdf
+pdfunite pub-Euler2024-MetronomeSpin-bw{1,2,3,4,5,6,7,8,9,10,11}.pdf ../pub-Euler2024-MetronomeSpin.pdf
+# this is already black-and-white
+cp pub-Franz2022-Cusp.pdf ../pub-Franz2022-Cusp.pdf
